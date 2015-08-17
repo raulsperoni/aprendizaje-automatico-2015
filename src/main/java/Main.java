81999cc99bbd9468ceafaeafcf42624d7c86e988
raulsperoni <raulsperoni@gmail.com>
2015-08-17 13:25:13 -0300

    Primer Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.logging.Logger;
+
+/**
+ * Created by RSperoni on 17/08/2015.
+ */
+public class Main {
+
+    public static void main(String [ ] args){
+
+        Logger.getAnonymousLogger().info("MAA 2015");
+
+    }
+
+}

f9040f595d2eee296d1eee538f6158dc46f39069
raul <raul@mgcoders.uy>
2015-08-28 20:25:13 -0300

    particiones

--- src/main/java/practico2/Main.java
+++ src/main/java/practico2/Main.java
@@ -1,5 +1,6 @@
 package practico2;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,15 +9,41 @@ import java.util.List;
  */
 public class Main {
 
+
     public static void main(String[] args) {
 
         List<Ejemplo> ejemplos = AuxLoadData.retreive();
 
-        //ESTO ES SIN REF CRUZADA.
         Collections.shuffle(ejemplos);
         int corte = (ejemplos.size() * 4) / 5;
 
-        List<Ejemplo> entrenamiento = ejemplos.subList(0, corte);
+        List<Ejemplo> entrenamiento_total = ejemplos.subList(0, corte);
+
+        //VALIDACION CRUZADA
+        ArrayList<List<Ejemplo>> particiones = new ArrayList<>();
+        int ini_particion = 0;
+        for (int i = 0; i < 10; i++) {
+            particiones.add(entrenamiento_total.subList(ini_particion, ini_particion + 10));
+            ini_particion += 10;
+        }
+
+        //PARA CADA PARTICION
+        for (int i = 0; i < 10; i++) {
+
+            List<Ejemplo> prueba_validacion_cruzada = particiones.get(i);
+            List<Ejemplo> entrenamiento_validacion_cruzada = new ArrayList<>();
+            //Hago una copia del conjunto de entrenamiento.
+            Collections.copy(entrenamiento_validacion_cruzada, entrenamiento_total);
+            //Le saco el conjunto de prueba actual.
+            entrenamiento_validacion_cruzada.removeAll(prueba_validacion_cruzada);
+
+            //Llamo a ID3 con el conjunto de entrenamiento.
+
+            //Evaluo el conj de prueba con el resultado de ID3
+
+            //Hago algo con los valores resultantes, media, etc.
+
+        }
 
 
     }

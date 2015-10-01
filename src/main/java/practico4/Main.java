88df25d2bd4ee442c9c3c5c8396a993038e5ec1f
raul <raul@mgcoders.uy>
2015-10-01 19:02:13 -0300

    ta, andan cosas

--- src/main/java/practico4/Main.java
+++ src/main/java/practico4/Main.java
@@ -20,7 +20,7 @@ public class Main {
             funcionPotencia4();
 
             //f(x) = cos(7/2*pi*x)
-            funcionCoseno();
+            //funcionCoseno();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -34,17 +34,40 @@ public class Main {
         List<Double> salidas_esperadas_funcion1 = new ArrayList<>();
         Random r = new Random();
         for (int i = 0; i < 500; i++) {
-            double x = Util.randDouble(0, 1, r);
+            double x = Util.randDouble(-1, 1, r);
             entradas_funcion1.add(i, new ArrayList<Double>(Arrays.asList(x)));
             salidas_esperadas_funcion1.add(i, x);
         }
-        RedNeuronal redNeuronal = new RedNeuronal<>(2, 1, 1, 0.1, 40000, Sigmoid.class, 0d);
+        int iteraciones = 50000;
+        Class cls = Tanh.class;
+        RedNeuronal redNeuronal = new RedNeuronal<>(6, 1, 1, 0.1, iteraciones, cls, 0d);
         HashMap<Integer, Double> err = redNeuronal.backpropagation(entradas_funcion1, salidas_esperadas_funcion1);
         Double minErr = err.get(err.size() - 1);
         Util.Plot(err, "Indentidad: min:" + String.format("%.3f", minErr), 0, err.size());
-        double ejemploAEvaluar = Util.randDouble(-1, 1, r);
-        List<Double> resultado = redNeuronal.evaluar(new ArrayList<Double>(Arrays.asList(ejemploAEvaluar)));
-        System.out.println("Ejemplo Identidad: " + ejemploAEvaluar + "\t" + "Resultado: " + resultado.get(0));
+
+        /**
+         * Evaluacion
+         */
+
+        List<Double> entradasEval = new ArrayList<>();
+        List<Double> salidasExactasEval = new ArrayList<>();
+        List<Double> salidasRedEval = new ArrayList<>();
+        r = new Random();
+        for (int i = 0; i < 100; i++) {
+            double x = Util.randDouble(-1, 1, r);
+            entradasEval.add(i, x);
+            //if (x < 0) x = -x;
+            List<Double> resultado = redNeuronal.evaluar(new ArrayList<Double>(Arrays.asList(x)));
+            salidasExactasEval.add(i, x);
+            double res = resultado.get(0);
+            //if (x < 0)
+            //    salidasRedEval.add(i,-res);
+            //else
+            salidasRedEval.add(i, res);
+        }
+        Util.Plot(entradasEval, salidasExactasEval, salidasRedEval, "Identidad", iteraciones, cls);
+
+
     }
 
     public static void funcionPotencia4() throws Exception {
@@ -53,17 +76,35 @@ public class Main {
         List<Double> salidas_esperadas_funcion = new ArrayList<>();
         Random r = new Random();
         for (int i = 0; i < 500; i++) {
-            double x = Util.randDouble(-1, 1, r);
-            entradas_funcion.add(i, new ArrayList<Double>(Arrays.asList(x)));
-            salidas_esperadas_funcion.add(i, Math.pow(x, 4d));
+            double x = Util.randDouble(0, 1, r);
+            entradas_funcion.add(i, new ArrayList<Double>(Arrays.asList(Math.abs(x))));
+            salidas_esperadas_funcion.add(i, Math.pow(Math.abs(x), 4));
         }
-        RedNeuronal redNeuronal = new RedNeuronal(2, 1, 1, 0.1, 100000, Tanh.class, 0d);
+        int iteraciones = 50000;
+        Class cls = Sigmoid.class;
+        RedNeuronal redNeuronal = new RedNeuronal(6, 1, 1, 0.1, iteraciones, cls, 0d);
         HashMap<Integer, Double> err = redNeuronal.backpropagation(entradas_funcion, salidas_esperadas_funcion);
         Double minErr = err.get(err.size() - 1);
-        Util.Plot(err, "Potencia: min:" + String.format("%.3f", minErr), 70000, err.size());
-        double ejemploAEvaluar = Util.randDouble(-1, 1, r);
-        List<Double> resultado = redNeuronal.evaluar(new ArrayList<Double>(Arrays.asList(ejemploAEvaluar)));
-        System.out.println("Ejemplo Potencia: " + ejemploAEvaluar + "\t" + "Resultado: " + Math.sqrt(Math.sqrt(resultado.get(0))));
+        Util.Plot(err, "Potencia: min:" + String.format("%.3f", minErr), 0, err.size());
+
+
+        /**
+         * Evaluacion
+         */
+
+        List<Double> entradasEval = new ArrayList<>();
+        List<Double> salidasExactasEval = new ArrayList<>();
+        List<Double> salidasRedEval = new ArrayList<>();
+        r = new Random();
+        for (int i = 0; i < 100; i++) {
+            double x = Util.randDouble(0, 1, r);
+            entradasEval.add(i, x);
+            List<Double> resultado = redNeuronal.evaluar(new ArrayList<Double>(Arrays.asList(x)));
+            salidasExactasEval.add(i, Math.pow(x, 4));
+            double res = resultado.get(0);
+            salidasRedEval.add(i, res);
+        }
+        Util.Plot(entradasEval, salidasExactasEval, salidasRedEval, "Potencia", iteraciones, cls);
 
     }
 
@@ -74,15 +115,15 @@ public class Main {
         for (int i = 0; i < 500; i++) {
             double x = Util.randDouble(-1, 1, r);
             entradas_funcion.add(i, new ArrayList<Double>(Arrays.asList(x)));
-            salidas_esperadas_funcion.add(i, Math.cos((7 / 2) * Math.PI * x));
+            salidas_esperadas_funcion.add(i, (Math.cos((7 / 2) * Math.PI * x) + 1) / 2);
         }
-        RedNeuronal redNeuronal = new RedNeuronal(2, 1, 1, 0.1, 50000, Tanh.class, 0d);
+        RedNeuronal redNeuronal = new RedNeuronal(2, 1, 1, 0.1, 50000, Sigmoid.class, 0d);
         HashMap<Integer, Double> err = redNeuronal.backpropagation(entradas_funcion, salidas_esperadas_funcion);
         Double minErr = err.get(err.size() - 1);
-        Util.Plot(err, "Coseno: min:" + String.format("%.3f", minErr), 0, err.size());
+        Util.Plot(err, "Coseno: min:" + String.format("%.3f", minErr), 0, 500);
         double ejemploAEvaluar = Util.randDouble(-1, 1, r);
         List<Double> resultado = redNeuronal.evaluar(new ArrayList<Double>(Arrays.asList(ejemploAEvaluar)));
-        System.out.println("Ejemplo Coseno: " + ejemploAEvaluar + "\t" + "Resultado: " + Math.acos((resultado.get(0) / Math.PI) * (2 / 7)));
+        System.out.println("Ejemplo Coseno: " + ejemploAEvaluar + "\t" + "Resultado: " + Math.acos(((resultado.get(0) * 2) - 1 / Math.PI) * (2 / 7)));
 
 
     }
--- src/main/java/practico4/Util.java
+++ src/main/java/practico4/Util.java
@@ -9,6 +9,7 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -45,6 +46,36 @@ public class Util {
         int width = 1024; /* Width of the image */
         int height = 768; /* Height of the image */
         File XYChart = new File("ErrPlot" + nom + ".jpeg");
+        ChartUtilities.saveChartAsJPEG(XYChart, xylineChart, width, height);
+    }
+
+    public static void Plot(List<Double> ejex, List<Double> ejeyexacto, List<Double> ejeyRed, String nom, int iteraciones, Class cls) throws Exception {
+        final XYSeries exacto = new XYSeries("Exacto");
+        for (int i = 0; i < ejex.size(); i++) {
+            exacto.add(ejex.get(i), ejeyexacto.get(i));
+        }
+
+        final XYSeries red = new XYSeries("Red");
+        for (int i = 0; i < ejex.size(); i++) {
+            red.add(ejex.get(i), ejeyRed.get(i));
+        }
+
+        final XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(exacto);
+        dataset.addSeries(red);
+
+
+        JFreeChart xylineChart = ChartFactory.createXYLineChart(
+                "E -" + nom,
+                "Iteracion",
+                "Error",
+                dataset,
+                PlotOrientation.VERTICAL,
+                true, true, false);
+
+        int width = 1024; /* Width of the image */
+        int height = 768; /* Height of the image */
+        File XYChart = new File("Func" + nom + iteraciones + cls.getName() + ".jpeg");
         ChartUtilities.saveChartAsJPEG(XYChart, xylineChart, width, height);
     }
 

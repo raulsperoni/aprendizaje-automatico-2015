626fea3c7814e6a3e9d6b94d03cea974088499bd
raul <raul@mgcoders.uy>
2015-10-04 19:09:23 -0300

    start

--- src/main/java/practico3/comun/Algoritmo.java
+++
@@ -1,9 +0,0 @@
-package practico3.comun;
-
-/**
- * Created by RSperoni on 14/09/2015.
- */
-public enum Algoritmo {
-
-    ID3, KNN, NAIVEBAYES
-}
--- src/main/java/practico3/id3/Hoja.java
+++
@@ -1,9 +0,0 @@
-package practico3.id3;
-
-/**
- * Created by raul on 30/08/15.
- */
-public class Hoja {
-
-    public boolean poisonus;
-}
